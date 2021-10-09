b0fdfa0a984f484e2539e0a5ea1f42f31b684f3d
BananeChocolat <jy.aurelien@gmail.com>
2021-10-09 21:44:15 +0200

    ast commit

---
+++ NavalWar_JACQUES_DIYAN_HALLER.py
@@ -0,0 +1,13 @@
+### aure
+grille = [[0 for _ in range(10)] for _ in range(10)]
+
+print(grille)
+
+
+def afficheGrille(grille):
+    print(grille)
+
+
+
+
+### tibo
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NSIproject-NavalWar
  Naval war game
+ 
+ ![asuka en bateau](https://giffiles.alphacoders.com/112/112282.gif)
