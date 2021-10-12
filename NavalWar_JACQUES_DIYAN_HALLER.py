e9bd4a9475957e788f8ef7c4c3da73299b835688
BananeChocolat <jy.aurelien@gmail.com>
2021-10-12 15:22:09 +0200

    added placeBoat()

--- NavalWar_JACQUES_DIYAN_HALLER.py
+++ NavalWar_JACQUES_DIYAN_HALLER.py
@@ -1,13 +1,28 @@
+from pprint import pprint
 ### aure
-grille = [[0 for _ in range(10)] for _ in range(10)]
+grille = [['O' for _ in range(10)] for _ in range(10)]
 
-print(grille)
+
 
 
 def afficheGrille(grille):
-    print(grille)
+    pprint(grille)
 
+def placeBoat(len:int, pos:tuple, dir=None):
+    
+    assert pos[0] or pos[1] <= len(grille[0]), "position hors de la grille"
+    assert pos[0]+len-1 or pos[1]+len-1 <= len(grille[0]), "le beateau sors de la grille"
 
+    if dir =='horz':
+        for i in range(len):
+            grille[pos[1]-1][pos[0]-1+i] = 'X'
+    elif dir == 'vert':
+        for i in range(len):
+            grille[pos[1]-1+i][pos[0]-1] = 'X'
+        
 
+placeBoat(3, (2,2),'vert')
+
+afficheGrille(grille)
 
 ### tibo
