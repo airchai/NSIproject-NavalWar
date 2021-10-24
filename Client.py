7c1b8807efab74cb86f38f57b343408372969c27
crocogab <50633541+crocogab@users.noreply.github.com>
2021-10-24 15:29:45 +0200

    update server

--- Client.py
+++ Client.py
@@ -2,7 +2,7 @@ import socket
 import sys
 from time import sleep
 
-HOST, PORT = "localhost", 3288
+HOST, PORT = "localhost", 2004
 data = "hello" #donnees a envoyer
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #def socket
@@ -11,10 +11,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #def socket
 sock.connect((HOST, PORT))
 while True:
     a=input()
-    if a==a:
+    if a=='a':
         sock.send(data.encode('utf8'))
         received = sock.recv(1024)
         print(received)
         sleep(3)
     else:
-        sock.close
+        sock.shutdown(1)
+        break
--- Server.py
+++ Server.py
@@ -1,22 +1,50 @@
-import socketserver
-import threading
+import socket
+import os
+from _thread import *
 
+ServerSideSocket = socket.socket()
+host = '127.0.0.1'
+port = 2004
+ThreadCount = 0
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
 
-class MyTCPHandler(socketserver.BaseRequestHandler): #cree class mytcpHandler (le serveur reste ouvert)
-    def handle(self):
-        while 1: #tjrs
-            self.data = self.request.recv(1024) #data max recues ( en bytes)
-            if not self.data: #quitte si ne sont pas donnees recues 
-                break
-            self.data = self.data.strip() #formalise le texte
-            print (str(self.client_address[0]) + " wrote: ","port:",self.client_address[1]) #dis qui a ecris quoi
-            print (self.data) #donnees
-            print(self.data.decode('utf-8')) #données decodées en UTF-8
-            self.request.send(self.data.upper()) #envoies donnees recus en majusucule
+print('Socket is listening..')
+ServerSideSocket.listen(5)
 
+def multi_threaded_client(connection):
+    global ThreadCount
+    connection.send(str.encode('Server is working:'))
+    while True:
+        try:
+            data = connection.recv(2048)
+            response = 'Server message: ' + data.decode('utf-8')
+            print(data.decode('utf-8'))
+            data_list.append(data.decode('utf-8'))
+        except socket.error:
+            ThreadCount -=1
+            pass
+        if not data:
+            ThreadCount -=1
+            break
+        try:
+            connection.sendall(str.encode(response))
+        except socket.error:
+            ThreadCount -=1
+            pass
+        
+    connection.close()
 
-if __name__ == "__main__": #execute si c le fichier principal 
-    HOST, PORT = "localhost", 3288 #parametre HOST + PORT (>1083 = pas secu os)
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler) #parametre le serveur avec les fonctionnalités utiles
-    server.serve_forever() #receptionne les demandes tant que pas de shutdown()
+data_list=[]
+while True:
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+    data_list = list(filter(None, data_list))
+    print(data_list)
+
 
