ef66251aa38d3ba6846ef703cda9bf0fe6f9a6d0
crocogab <50633541+crocogab@users.noreply.github.com>
2021-10-24 15:56:45 +0200

    update serveur

--- Client.py
+++ Client.py
@@ -9,12 +9,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #def socket
 
 
 sock.connect((HOST, PORT))
-while True:
+while True: 
     a=input()
-    if a=='a':
+    if a=='a':#envoyer les donnees si input=a
         sock.send(data.encode('utf8'))
-        received = sock.recv(1024)
-        print(received)
+        received = sock.recv(2048)
+        print(received) #afficher reponse serveur
         sleep(3)
     else:
         sock.shutdown(1)
--- Server.py
+++ Server.py
@@ -2,49 +2,50 @@ import socket
 import os
 from _thread import *
 
-ServerSideSocket = socket.socket()
-host = '127.0.0.1'
-port = 2004
-ThreadCount = 0
+ServerSideSocket = socket.socket() #cree un socket avec librairie
+host = '127.0.0.1' #pour instant localhost mais bientot heberge sur pi+ port forwarding
+port = 2004 
+ThreadCount = 0 #compteur de connections
 try:
-    ServerSideSocket.bind((host, port))
+    ServerSideSocket.bind((host, port)) # essaye de bind le serveir et si marche pas renvoie erreur 
 except socket.error as e:
     print(str(e))
 
-print('Socket is listening..')
-ServerSideSocket.listen(5)
+print('Socket is listening..') 
+ServerSideSocket.listen(5) #serveur attends connections (écoute)
 
 def multi_threaded_client(connection):
-    global ThreadCount
-    connection.send(str.encode('Server is working:'))
-    while True:
-        try:
-            data = connection.recv(2048)
-            response = 'Server message: ' + data.decode('utf-8')
-            print(data.decode('utf-8'))
-            data_list.append(data.decode('utf-8'))
-        except socket.error:
-            ThreadCount -=1
+    """Permet la connection de plusieurs clients sur le même serveur"""
+    global ThreadCount # récupère la variable threadcount
+    connection.send(str.encode('Server is working:')) # A la connection du client le serveur renvoie qu'il fonctionne
+    while True: 
+        try: #execute tant que client est connecté 
+            data = connection.recv(2048) #reçois la data du client
+            response = 'Server message: ' + data.decode('utf-8') #réponse
+            print(data.decode('utf-8')) #affiche la data décodée en UTF-8
+            data_list.append(data.decode('utf-8')) #ajoute a la liste de data les datas (va bientot etre utilisées pour les grilles)
+        except socket.error:#si error threadcount - 1 car on perd un client
+            ThreadCount -=1 
             pass
-        if not data:
+        if not data: #si error threadcount - 1 car on perd un client
             ThreadCount -=1
             break
         try:
-            connection.sendall(str.encode(response))
-        except socket.error:
+            connection.sendall(str.encode(response)) #essaye de renvoyer une reponse au client
+        except socket.error: #si error threadcount - 1 car on perd un client
             ThreadCount -=1
             pass
         
-    connection.close()
+    connection.close() #ferme connection quand fini
 
 data_list=[]
 while True:
-    Client, address = ServerSideSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(multi_threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-    data_list = list(filter(None, data_list))
-    print(data_list)
+    Client, address = ServerSideSocket.accept() #accepte les connections
+    print('Connected to: ' + address[0] + ':' + str(address[1])) #affiche les ip / port des clients connectes 
+    start_new_thread(multi_threaded_client, (Client, )) #lance la connection de plusieurs clients sur 1 serveur
+    ThreadCount += 1 #ajoute 1 client
+    print('Thread Number: ' + str(ThreadCount)) #affiche nombre clients connectes
+    data_list = list(filter(None, data_list)) #retire vide de la data_list (genere quand client se deconnecte)
+    print(data_list) #affiche la data en memoire
 
 
